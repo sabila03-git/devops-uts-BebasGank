f2ddee2c2948b5e02c2d1fd77bbc43005070c701
Salsa Billa Aurilia <salsabillaaurl@students.amikom.ac.id>
2026-05-26 08:13:11 +0700

    Menambahkan konfigurasi IaC dengan Terraform

---
+++ terraform/main.tf
@@ -0,0 +1,63 @@
+# Konfigurasi Terraform
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+  required_version = ">= 1.0"
+}
+
+# Provider Docker
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+# Resource: Pull image aplikasi kita
+resource "docker_image" "app_image" {
+  name         = "node:18-alpine"
+  keep_locally = false
+}
+
+# Resource: Container Redis (untuk caching)
+resource "docker_container" "redis" {
+  name  = "redis-devops-uts"
+  image = "redis:alpine"
+
+  ports {
+    internal = 6379
+    external = 6379
+  }
+
+  restart = "unless-stopped"
+}
+
+# Resource: Container aplikasi web kita
+resource "docker_container" "web_app" {
+  name  = "webapp-devops-uts"
+  image = docker_image.app_image.image_id
+
+ports {
+  internal = 3000
+  external = 8081
+}
+
+  env = [
+    "NODE_ENV=production",
+    "PORT=3000"
+  ]
+
+  restart = "unless-stopped"
+}
+
+# Output informasi container
+output "redis_container_name" {
+  value       = docker_container.redis.name
+  description = "Nama container Redis"
+}
+
+output "webapp_container_name" {
+  value       = docker_container.web_app.name
+  description = "Nama container Web App"
+}
